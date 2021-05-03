9cc189b16841ca95ed9123c03b5985c441e7ca10
Kakomo <kakomo2@gmail.com>
2021-05-03 14:29:35 -0300

    Codigo Inicial

---
+++ lib/classes/app_images.dart
@@ -0,0 +1,13 @@
+class AppImages {
+
+  static String get um => "assets/um.png";
+  static String get dois => "assets/dois.png";
+  static String get tres => "assets/tres.png";
+  static String get quatro => "assets/quatro.png";
+  static String get cinco => "assets/cinco.png";
+  static String get seis => "assets/seis.png";
+  static String get sete => "assets/sete.png";
+  static String get oito => "assets/oito.png";
+  static String get nove => "assets/nove.png";
+
+}
