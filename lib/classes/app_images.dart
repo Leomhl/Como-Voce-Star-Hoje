8b2a3c65d94c16b8dccb3296c35efee81b6c2f89
Kakomo <kakomo2@gmail.com>
2021-05-03 14:50:47 -0300

    Codigo Inicial

--- lib/classes/app_images.dart
+++ lib/classes/app_images.dart
@@ -1,13 +1,13 @@
 class AppImages {
 
-  static String get um => "assets/um.png";
-  static String get dois => "assets/dois.png";
-  static String get tres => "assets/tres.png";
-  static String get quatro => "assets/quatro.png";
-  static String get cinco => "assets/cinco.png";
-  static String get seis => "assets/seis.png";
-  static String get sete => "assets/sete.png";
-  static String get oito => "assets/oito.png";
-  static String get nove => "assets/nove.png";
+  static String get um => "assets/um.jpg";
+  static String get dois => "assets/dois.jpg";
+  static String get tres => "assets/tres.jgp";
+  static String get quatro => "assets/quatro.jpg";
+  static String get cinco => "assets/cinco.jpg";
+  static String get seis => "assets/seis.jpg";
+  static String get sete => "assets/sete.jpg";
+  static String get oito => "assets/oito.jpg";
+  static String get nove => "assets/nove.jpg";
 
 }
