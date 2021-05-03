aaa0ec155b5fa1b35160da485139fd848bb8687a
Leonardo Marinho <leonardomarinho.nf@gmail.com>
2021-05-03 15:51:56 -0300

    Tela principal

--- lib/classes/app_images.dart
+++ lib/classes/app_images.dart
@@ -1,13 +1,13 @@
 class AppImages {
 
-  static String get um => "assets/um.png";
-  static String get dois => "assets/dois.png";
-  static String get tres => "assets/tres.png";
-  static String get quatro => "assets/quatro.png";
-  static String get cinco => "assets/cinco.png";
-  static String get seis => "assets/seis.png";
-  static String get sete => "assets/sete.png";
-  static String get oito => "assets/oito.png";
-  static String get nove => "assets/nove.png";
+  static String get um => "assets/images/1.jpg";
+  static String get dois => "assets/images/dois.png";
+  static String get tres => "assets/images/tres.png";
+  static String get quatro => "assets/images/quatro.png";
+  static String get cinco => "assets/images/cinco.png";
+  static String get seis => "assets/images/seis.png";
+  static String get sete => "assets/images/sete.png";
+  static String get oito => "assets/images/oito.png";
+  static String get nove => "assets/images/nove.png";
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,15 @@
 import 'package:como_voce_star_hoje/screens/1.dart';
+import 'package:como_voce_star_hoje/screens/2.dart';
+import 'package:como_voce_star_hoje/screens/3.dart';
+import 'package:como_voce_star_hoje/screens/4.dart';
+import 'package:como_voce_star_hoje/screens/5.dart';
+import 'package:como_voce_star_hoje/screens/6.dart';
+import 'package:como_voce_star_hoje/screens/7.dart';
+import 'package:como_voce_star_hoje/screens/8.dart';
+import 'package:como_voce_star_hoje/screens/9.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'classes/app_images.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,36 +20,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Como Você Star Hoje?',
+      title: 'StarToday',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Um(),
+      home: MyHomePage(title: 'StarToday'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -47,52 +39,60 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  List<Map> items = [
+    {'img': AppImages.um, 'screen': Um()},
+    {'img': AppImages.um, 'screen': Dois()},
+    {'img': AppImages.um, 'screen': Tres()},
+    {'img': AppImages.um, 'screen': Quatro()},
+    {'img': AppImages.um, 'screen': Cinco()},
+    {'img': AppImages.um, 'screen': Seis()},
+    {'img': AppImages.um, 'screen': Sete()},
+    {'img': AppImages.um, 'screen': Oito()},
+    {'img': AppImages.um, 'screen': Nove()},
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Align(
+          alignment: Alignment.center,
           child: GridView.count(
-            // Create a grid with 2 columns. If you change the scrollDirection to
-            // horizontal, this produces 2 rows.
+            shrinkWrap: true,
             crossAxisCount: 3,
-            // Generate 100 widgets that display their index in the List.
-            children: List.generate(9, (index) {
-              return Padding(
-                padding:
-                    EdgeInsets.only(top: 10, bottom: 10, left: 5, right: 5),
-                child: Center(
-                    child:
-                        Image.network('https://i.redd.it/h2vj5cpoghe11.jpg')),
-              );
-            }),
+            children: _generateItems(items, context),
           ),
-        )
-        );
+        ),
+      ),
+    );
   }
+}
+
+List<Widget> _generateItems(List<Map> items, BuildContext context) {
+  List<Widget> list = [];
+
+  items.forEach((item) => list.add(
+    GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => item['screen'])
+        );
+      },
+      child: Padding(
+        padding: EdgeInsets.all(2),
+        child: Image.asset(
+          item['img'],
+          fit: BoxFit.cover,
+        ),
+      ),
+    ),
+  ));
+
+  return list;
 }
